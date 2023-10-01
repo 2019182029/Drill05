1bae1c1cd9b076e8431e5a0824677ea663f40fec
2019182029_이원준 <S159753S@tukorea.ac.kr>
2023-10-01 14:02:41 +0900

    added character moving and quit
    added character moving, SLD_QUIT and SDLK_ESCAPE to quit program

---
+++ Drill05.py
@@ -0,0 +1,32 @@
+from pico2d import *
+
+ground_width, ground_height = 1280, 1024
+running = True
+frame = 0
+
+open_canvas(ground_width, ground_height)
+
+ground = load_image('TUK_GROUND.png')
+character = load_image('character.png')
+
+def handle_events() :
+    global running
+    
+    events = get_events()
+    for event in events :
+        if(event.type == SDL_QUIT) :
+            running = False
+        elif(event.type == SDL_KEYDOWN) :
+            if(event.key == SDLK_ESCAPE) :
+                running = False
+
+while(running) :
+    clear_canvas()
+    ground.draw(ground_width // 2, ground_height // 2)
+    character.clip_draw(frame * 100, 0, 100, 100, ground_width // 2, ground_height // 2)
+    update_canvas()
+    handle_events()
+    
+    frame = (frame + 1) % 8
+
+close_canvas()
