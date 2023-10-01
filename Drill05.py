ab07ce87d61a0a9a9bb7751f271674761c827ff5
2019182029_이원준 <S159753S@tukorea.ac.kr>
2023-10-01 14:19:23 +0900

    added character moving to hand

--- Drill05.py
+++ Drill05.py
@@ -4,8 +4,8 @@ import random
 ground_width, ground_height = 1280, 1024
 running = True
 frame = 0
-hand_x, hand_y = 0, 0
-cha_x, cha_y = 0, 0
+hand_x, hand_y = ground_width // 2, ground_height // 2
+cha_x, cha_y = ground_width // 2, ground_height // 2
 
 open_canvas(ground_width, ground_height)
 
@@ -26,20 +26,27 @@ def handle_events() :
     
 
 while(running) :
-    clear_canvas()
+    for i in range(0, 100 + 1, 4) :
+        clear_canvas()
 
-    ground.draw(ground_width // 2, ground_height // 2)
-    character.clip_draw(frame * 100, 0, 100, 100, ground_width // 2, ground_height // 2)
+        ground.draw(ground_width // 2, ground_height // 2)
+        character.clip_draw(frame * 100, 0, 100, 100, cha_x, cha_y)
 
-    if((hand_x, hand_y) == (cha_x, cha_y)) :
-        hand_x, hand_y = random.randint(0, ground_width), random.randint(0, ground_height)
-    hand.draw(hand_x, hand_y)
+        if((hand_x, hand_y) == (cha_x, cha_y)) :
+            hand_x, hand_y = random.randint(0, ground_width), random.randint(0, ground_height)
+        hand.draw(hand_x, hand_y)
 
-    update_canvas()
-    handle_events()
-    
-    frame = (frame + 1) % 8
+        update_canvas()
 
-    delay(0.1)
+        t = i / 100
+
+        cha_x = (1 - t) * cha_x + t * hand_x
+        cha_y = (1 - t) * cha_y + t * hand_y
+
+        handle_events()
+        
+        frame = (frame + 1) % 8
+
+        delay(0.1)
 
 close_canvas()
